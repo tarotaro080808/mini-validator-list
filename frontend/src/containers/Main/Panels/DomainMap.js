16aa8b0d03d6c3a8eb15ec418f37e1a0b0a92b67
cinnapple <ysasaki.g.drive@gmail.com>
2018-08-07 21:36:13 -0700

    add: content placeholder

--- frontend/src/containers/Main/Panels/DomainMap.js
+++ frontend/src/containers/Main/Panels/DomainMap.js
@@ -1,7 +1,7 @@
 import React from "react";
+import ContentLoader from "react-content-loader";
 
 import { withStyles } from "@material-ui/core/styles";
-
 import ExpandablePanel from "../../../components/Common/ExpandablePanel";
 import DomainMapPanel from "../../../components/DomainMap/DomainMapPanel";
 
@@ -14,9 +14,33 @@ const styles = theme => ({
   }
 });
 
+const Loader = props => (
+  <ContentLoader
+    speed={2}
+    height={230}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+    preserveAspectRatio="xMidYMid slice"
+    {...props}
+  >
+    <rect x="0" y="0" rx="5" ry="5" width="100%" height="230" />
+  </ContentLoader>
+);
+
 class DomainMap extends React.Component {
   render() {
     const { classes, vals, isLoading } = this.props;
+    let content = Loader(this.props);
+
+    if (!isLoading && vals.uniqueDomains) {
+      content = (
+        <DomainMapPanel
+          domains={vals.uniqueDomains}
+          positions={vals.positions}
+        />
+      );
+    }
+
     return (
       <ExpandablePanel
         className={classes.panel}
@@ -24,14 +48,7 @@ class DomainMap extends React.Component {
         expanded={false}
       >
         <div className={classes.wrapper} key="DomainMap">
-          {!isLoading && vals.uniqueDomains ? (
-            <DomainMapPanel
-              domains={vals.uniqueDomains}
-              positions={vals.positions}
-            />
-          ) : (
-            <React.Fragment />
-          )}
+          {content}
         </div>
       </ExpandablePanel>
     );
--- frontend/src/containers/Main/Panels/ValidatorList.js
+++ frontend/src/containers/Main/Panels/ValidatorList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ContentLoader from "react-content-loader";
 
 import { withStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
@@ -11,18 +12,39 @@ const styles = theme => ({
   }
 });
 
+const Loader = props => (
+  <div style={{ padding: "1rem" }}>
+    <ContentLoader
+      speed={2}
+      height={250}
+      primaryColor="#f3f3f3"
+      secondaryColor="#ecebeb"
+      preserveAspectRatio="xMidYMid slice"
+      {...props}
+    >
+      <rect x="0" y="25" rx="0" ry="5" width="100%" height="5" />
+      <rect x="0" y="55" rx="0" ry="5" width="100%" height="5" />
+      <rect x="0" y="85" rx="0" ry="5" width="100%" height="5" />
+      <rect x="0" y="115" rx="0" ry="5" width="100%" height="5" />
+      <rect x="0" y="145" rx="0" ry="5" width="100%" height="5" />
+      <rect x="0" y="175" rx="0" ry="5" width="100%" height="5" />
+      <rect x="0" y="205" rx="0" ry="5" width="100%" height="5" />
+    </ContentLoader>
+  </div>
+);
+
 class ValidatorList extends React.Component {
   render() {
     const { classes, vals, isLoading } = this.props;
+
+    let content = Loader(this.props);
+    if (!isLoading && vals.filteredValidators) {
+      content = <Table list={vals.filteredValidators} />;
+    }
+
     return (
       <div className={classes.wrapper} key="ValidatorList">
-        <Paper>
-          {!isLoading && vals.filteredValidators ? (
-            <Table list={vals.filteredValidators} />
-          ) : (
-            <React.Fragment />
-          )}
-        </Paper>
+        <Paper>{content}</Paper>
       </div>
     );
   }
