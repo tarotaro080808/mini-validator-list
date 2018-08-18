b43d28f138ce552ec95c0e0be8c4fe336c28efdf
Prosatan666 <42497000+Prosatan666@users.noreply.github.com>
2018-08-18 19:30:56 +0300

    Added Russian language
    Twitter @Prosatan666

--- frontend/src/resources.ts
+++ frontend/src/resources.ts
@@ -3,44 +3,32 @@ const res = {
   DONATION_LINK: {
     en: "https://www.xrptipbot.com/u:CinnappleFun/n:twitter"
   },
-  HOME: { en: "Home", ja: "ホーム" },
-  DONATE: { en: "Donate", ja: "寄付" },
-  FILTER: { en: "Filter", ja: "フィルター" },
-  DEFAULT_UNL_ONLY: { en: "Default UNL Only", ja: "デフォルトUNLのみ" },
-  VERIFIED_DOMAINS_ONLY: {
-    en: "Verified Domains Only",
-    ja: "承認済みドメインのみ"
-  },
-  MAIN_NET_ONLY: { en: "Main Net Only", ja: "メイン・ネットのみ" },
-  FILTER_BY_DOMAIN_NAME: { en: "Filter by domain", ja: "ドメイン名で絞り込む" },
-  CLEAR_FILTER: {
-    en: "Reset",
-    ja: "リセット"
-  },
-  LOAD_ANOTHER_DEFAULT_UNL: {
-    en: "Load Another Default UNL",
-    ja: "他のデフォルトUNLを読み込む"
-  },
-  STATS: { en: "Stats", ja: "統計情報" },
-  STATS_VALIDATORS: { en: "validators", ja: "バリデータ" },
-  STATS_IN_DEFAULT_UNL: { en: "in default UNL", ja: "デフォルトUNL" },
-  STATS_RIPPLE: { en: "ripple.com", ja: "リップル社運営" },
-  STATS_DOMINANCE: { en: "dominance", ja: "ドミナンス" },
-  DOMAIN_MAP: { en: "Domain Map", ja: "ドメイン・マップ" },
-  LOCATE_DOMAIN: { en: "Locate Domain", ja: "ドメイン名から場所を特定する" },
-  SETTINGS: { en: "Settings", ja: "設定" },
-  SETTINGS_APPEARANCE: { en: "Appearance", ja: "外観の設定" },
-  LANGUAGE_SETTINGS: { en: "Language / 言語", ja: "言語 / Language" },
-  SETTINGS_THEME: { en: "Current Theme", ja: "現在のテーマ" },
-  THEME_DARK: { en: "Dark", ja: "ダーク" },
-  THEME_LIGHT: { en: "Light", ja: "ライト" },
-  VALIDATOR_LIST_COL_DOMAIN: { en: "Domain", ja: "ドメイン" },
-  VALIDATOR_LIST_COL_PUBKEY: {
-    en: "Validation Public Key",
-    ja: "バリデータ 公開鍵"
-  },
-  DOMAIN_UNVERIFIED: { en: "unverified", ja: "未承認ドメイン" },
-  MENU_GET_IN_TOUCH: { en: "Get in touch", ja: "作者にコンタクト" }
+  HOME: { en: "Home", ja: "ホーム", ru:"Главная" },
+  DONATE: { en: "Donate", ja: "寄付", ru:"Помочь проекту" },
+  FILTER: { en: "Filter", ja: "フィルター", ru:"Фильтр" },
+  DEFAULT_UNL_ONLY: { en: "Default UNL Only", ja: "デフォルトUNLのみ", ru:"Только узлы по умолчанию" },
+  VERIFIED_DOMAINS_ONLY: { en: "Verified Domains Only", ja: "承認済みドメインのみ", ru:"Только проверенные домены"  },
+  MAIN_NET_ONLY: { en: "Main Net Only", ja: "メイン・ネットのみ", ru:"Только рабочая сеть" },
+  FILTER_BY_DOMAIN_NAME: { en: "Filter by domain", ja: "ドメイン名で絞り込む", ru:"Фильтр доменов" },
+  CLEAR_FILTER: { en: "Reset", ja: "リセット", ru:"Сброс" },
+  LOAD_ANOTHER_DEFAULT_UNL: { en: "Load Another Default UNL", ja: "他のデフォルトUNLを読み込む", ru:"Прошлые списки узлов" },
+  STATS: { en: "Stats", ja: "統計情報", ru:"Статистика" },
+  STATS_VALIDATORS: { en: "validators", ja: "バリデータ", ru:"Валидаторы" },
+  STATS_IN_DEFAULT_UNL: { en: "in default UNL", ja: "デフォルトUNL", ru:"Узлы по умолчанию" },
+  STATS_RIPPLE: { en: "ripple.com", ja: "リップル社運営", ru:"ripple.com" },
+  STATS_DOMINANCE: { en: "dominance", ja: "ドミナンス", ru:"Доминирование" },
+  DOMAIN_MAP: { en: "Domain Map", ja: "ドメイン・マップ", ru:"Карта адресов" },
+  LOCATE_DOMAIN: { en: "Locate Domain", ja: "ドメイン名から場所を特定する", ru:"Выбрать домен" },
+  SETTINGS: { en: "Settings", ja: "設定", ru:"Настройки" },
+  SETTINGS_APPEARANCE: { en: "Appearance", ja: "外観の設定", ru:"Интерфейс" },
+  LANGUAGE_SETTINGS: { en: "Language / 言語", ja: "言語 / Language", ru:"Язык" },
+  SETTINGS_THEME: { en: "Current Theme", ja: "現在のテーマ", ru:"Текущая тема" },
+  THEME_DARK: { en: "Dark", ja: "ダーク", ru:"Темная" },
+  THEME_LIGHT: { en: "Light", ja: "ライト", ru:"Светлая" },
+  VALIDATOR_LIST_COL_DOMAIN: { en: "Domain", ja: "ドメイン", ru:"Домен" },
+  VALIDATOR_LIST_COL_PUBKEY: { en: "Validation Public Key", ja: "バリデータ 公開鍵", ru:"Публичный ключ валидатора" },
+  DOMAIN_UNVERIFIED: { en: "unverified", ja: "未承認ドメイン", ru:"Не подтвержден" },
+  MENU_GET_IN_TOUCH: { en: "Get in touch", ja: "作者にコンタクト", ru:"Контакты" }
 };
 
 export { res };
